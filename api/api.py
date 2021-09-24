6edd8a1d03ec1cc50f99e5b52d83163a1108c74d
dayana-18 <dayrz18@gmail.com>
2021-09-24 10:03:29 +0200

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,11 +1,21 @@
 import flask
+import json
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+file = 'alexandry/bibliotheque.json'
+
+with open(file) as file:
+        data = json.load(file) #as a dicto
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(data)
 
 app.run()
